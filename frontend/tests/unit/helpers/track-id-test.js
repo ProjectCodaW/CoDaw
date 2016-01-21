1ef6b201d8e7ad04d8ad3cc7362776fb19887a62
mattahorton <matthew.a.horton@gmail.com>
2016-01-20 17:09:14 -0800

    Add sound object per track

---
+++ frontend/tests/unit/helpers/track-id-test.js
@@ -0,0 +1,10 @@
+import { trackId } from '../../../helpers/track-id';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | track id');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = trackId(42);
+  assert.ok(result);
+});
