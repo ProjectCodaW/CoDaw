8e706f559e7c2df864b512521ae81b193b3e6034
= <azim.pradhan@gmail.com>
2016-01-23 01:09:21 -0800

    Modified deploy config to save space

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :pty, true
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'frontend/node_modules')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
