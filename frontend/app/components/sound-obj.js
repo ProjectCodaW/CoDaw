6bbc09bee527abedb577a29149dec379555f4942
felakuti4life <ethan.geller@gmail.com>
2016-01-29 15:03:32 -0800

    CDengine 1.2: Implemented Wavesurfer Overrides, Implemented pauses and resumes mid-buffer

--- frontend/app/components/sound-obj.js
+++ frontend/app/components/sound-obj.js
@@ -1,16 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  fname: 'default.wav',
-  id: 'aWave',
-  start: 0,
-  startCrop: 0,
-  endCrop: 0,
+  fname: 'default.wav', //url of the audio file we're loading
+  id: 'aWave', //name of the container to create
+  start: 0, //Tick that sound starts on in project
+  startCrop: 0, //how far into the sound we are cropped to
+  endCrop: 0, //how much of the end we've truncated
   soundColor: 'green',
   soundProgressColor: 'purple',
+  placeInCD: -1,
   wavesurfer: null,
+
+  /*MARK: OVERRIDES*/
   didInsertElement: function() {
-    console.log('hit!');
     this.wavesurfer = WaveSurfer.create({
       container: '#'+this.id,
       waveColor: this.soundColor,
@@ -18,14 +20,19 @@ export default Ember.Component.extend({
     });
 
     this.wavesurfer.load(this.fname);
+    this.placeInCD = CD.addSound(this.start, this.startCrop, this.endCrop, this.wavesurfer);
   },
+
   click: function() {
-    this.wavesurfer.play();
+    CD.play();
   },
+
+  /*MARK: ACTIONS*/
   actions: {
     init() {
       this.wavesurfer = WaveSurfer.create({
         container: '#'+this.id,
+        context: CD.ctx,
         waveColor: this.soundColor,
         progressColor: this.soundProgressColor
       });
@@ -46,6 +53,12 @@ export default Ember.Component.extend({
     setWaveColor(color) {
       this.wavesurfer.waveColor = color;
       this.soundColor = color;
+    },
+
+    onUpdate() {
+      if(this.placeInCD >= 0) {
+        CD.modifySound(this.placeInCD, this.start, this.startCrop, this.endCrop, this.wavesurfer);
+      }
     }
 
   }
