f658144ebe33cc2cd7abf38b73b8faa36d5fe8f5
= <azim.pradhan@gmail.com>
2016-02-11 19:44:29 -0800

    Revert "Merge pull request #27 from ProjectCodaW/mattahorton/track-component"
    This reverts commit ccc55f4fb497c6d49a8b5296df014345bb1d763e, reversing
    changes made to c96257b81ec2d34cb8607a2ade05941b0506efa0.

--- frontend/app/helpers/sound-obj-id.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function soundObjId(params/*, hash*/) {
-  return "sound-obj-" + params[0] + "-" + params[1];
-  
-  // returns 'sound-obj-tracknum-soundnum'
-}
-
-export default Ember.Helper.helper(soundObjId);
