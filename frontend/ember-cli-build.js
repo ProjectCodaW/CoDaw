6dfb9610b690fa1be92fdef82b117a16c05e8529
= <azim.pradhan@gmail.com>
2016-02-12 20:58:15 -0800

    Add vendor and frontend to minifyJS exclude list

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -5,6 +5,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    minifyJS: {
+        options: {
+        exclude: ['**/vendor.js', '**/frontend.js']
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
