998f1a25f03c958712e09b05ab36cbad3050129d
felakuti4life <ethan.geller@gmail.com>
2016-02-22 16:53:18 -0800

    fixed JQuery selector for track components

--- frontend/app/components/audio-track.js
+++ frontend/app/components/audio-track.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    id: function() { return "track-" + (Math.random() * 10000).toFixed(0)},
+    //id: function() { return "track-" + (Math.random() * 10000).toFixed(0)},
     didInsertElement: function(){
         // Scroll the mute and solo buttons as we scroll up and down
         var topOffset = parseInt($(".mute-solo").css('margin-top'));
@@ -19,21 +19,22 @@ export default Ember.Component.extend({
     },
 
   click: function(evt) {
+    console.log(this.$());
     console.log(this.id + ": i was clicked at " + evt.clientX + "!");
-    console.log("our div is at " + $("#" + this.id).offset().left);
+    console.log("our div is at " + this.$().offset().left);
     var pxPerTick= 0.01; //this will be gotten from the CD.view constant
     //this.sendAction("drawPing", pxPerTick * (evt.clientX - $(this).offset.x), "Rails A.B. Geller", "#444");
-    this.drawPing( (evt.clientX - $("#" + this.id).offset().left)/pxPerTick, "Rails A.B. Geller", "#444");
+    this.drawPing( (evt.clientX - this.$().offset().left)/pxPerTick, "Rails A.B. Geller", "#444");
   },
 
   drawPing(tick, userName, userColor) {
     console.log("i was called at tick " + tick +"!");
-    var me = $('#' + this.id);
-    console.log(me);
+    var me = this.$();
     var pxPerTick= 0.01; //this will be gotten from the CD.view constant
-    var div =  this.id + "ping" + tick;
-    me.append("<div id=''" + div + "' class='ping'></div>");
-    var ping = $('#' + div);
+    var div =  "ping" + tick;
+    me.append("<div id='" + div + "' class='ping'></div>");
+    var ping = Ember.$('#' + div);
+    console.log(ping);
     ping.css({"position": "absolute",
       "top":"0px",
       "left": tick*pxPerTick + "px",
