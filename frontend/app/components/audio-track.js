c6a035ce6833816ca09b6045ed0269c75678b661
felakuti4life <ethan.geller@gmail.com>
2016-02-22 17:46:37 -0800

    fixed ping selector

--- frontend/app/components/audio-track.js
+++ frontend/app/components/audio-track.js
@@ -19,53 +19,57 @@ export default Ember.Component.extend({
     },
 
   click: function(evt) {
-    console.log(this.$());
-    console.log(this.id + ": i was clicked at " + evt.clientX + "!");
-    console.log("our div is at " + this.$().offset().left);
+
     var pxPerTick= 0.01; //this will be gotten from the CD.view constant
     //this.sendAction("drawPing", pxPerTick * (evt.clientX - $(this).offset.x), "Rails A.B. Geller", "#444");
-    this.drawPing( (evt.clientX - this.$().offset().left)/pxPerTick, "Rails A.B. Geller", "#444");
+    this.drawPing( (evt.clientX - this.$().offset().left)/pxPerTick, "Rails A.B. Geller", "#A3E");
   },
 
   drawPing(tick, userName, userColor) {
     console.log("i was called at tick " + tick +"!");
     var me = this.$();
     var pxPerTick= 0.01; //this will be gotten from the CD.view constant
-    var div =  "ping" + tick;
+    var div =  "ping" + tick.toFixed(0);
     me.append("<div id='" + div + "' class='ping'></div>");
     var ping = Ember.$('#' + div);
-    console.log(ping);
-    ping.css({"position": "absolute",
-      "top":"0px",
-      "left": tick*pxPerTick + "px",
-      "height": "100%",
-      "width": "80px",
-      "opacity": 0.3,
-      "background-color": userColor
-    });
-    ping.append("<h4>"+ userName +"</h4>");
+    if(ping.is(':empty')) {
+      console.log(ping);
+      ping.css({
+        "position": "absolute",
+        "top": "0px",
+        "left": tick * pxPerTick + "px",
+        "height": "140px",
+        "width": "80px",
+        "opacity": 0.9,
+        "background-color": userColor
+      });
+
+      ping.append("<h4>" + userName + "</h4>");
 
 
-    //start animatin'
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    ping.addClass('animated fadeIn').one(animationEnd, function() {
-      ping.removeClass('animated fadeIn');
-      ping.addClass('animated flip').one(animationEnd, function() {
-        ping.removeClass('animated flip');
-        ping.addClass('animated flip').one(animationEnd, function() {
+      //start animatin'
+      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      ping.addClass('animated fadeIn').one(animationEnd, function () {
+        console.log('animation 1...');
+        ping.removeClass('animated fadeIn');
+        ping.addClass('animated flip').one(animationEnd, function () {
+          console.log('animation 2...');
           ping.removeClass('animated flip');
-          ping.addClass('animated flip').one(animationEnd, function() {
-            ping.removeClass('animated flip');
-            ping.addClass('animated flipOutY').one(animationEnd, function() {
-              ping.removeClass('animated flipOutY');
-              //I'm done
-              ping.remove();
+          ping.addClass('animated flipOutX').one(animationEnd, function () {
+            console.log('animation 3');
+            ping.removeClass('animated flipOutX');
+            ping.addClass('animated bounce').one(animationEnd, function () {
+              ping.removeClass('animated bounce');
+              ping.addClass('animated flipOutY').one(animationEnd, function () {
+                ping.removeClass('animated flipOutY');
+                //I'm done
+                ping.remove();
+              });
             });
           });
         });
       });
-    });
-
+    }
   },
 
   actions: {
